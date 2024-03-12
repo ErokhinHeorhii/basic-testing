42ace5d7b682135a28bedffc9a2d8f9043ad6332
George Erokhin <Georgii070684@gmail.com>
2024-03-12 12:20:08 +0300

    feat: add test for Calculator

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,33 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import {simpleCalculator, Action} from './index';
 
 describe('simpleCalculator tests', () => {
-  test('should add two numbers', () => {
-    // Write your test here
-  });
+    test('should add two numbers', () => {
+        expect(simpleCalculator({a: 1, b: 2, action: Action.Add})).toBe(3);
+    });
 
-  test('should subtract two numbers', () => {
-    // Write your test here
-  });
+    test('should subtract two numbers', () => {
+        expect(simpleCalculator({a: 2, b: 1, action: Action.Subtract})).toBe(1);
+    });
 
-  test('should multiply two numbers', () => {
-    // Write your test here
-  });
+    test('should multiply two numbers', () => {
+        expect(simpleCalculator({a: 500, b: 100, action: Action.Multiply})).toBe(50000);
+    });
 
-  test('should divide two numbers', () => {
-    // Write your test here
-  });
+    test('should divide two numbers', () => {
+        expect(simpleCalculator({a: 500, b: 100, action: Action.Divide})).toBe(5);
+    });
 
-  test('should exponentiate two numbers', () => {
-    // Write your test here
-  });
+    test('should exponentiate two numbers', () => {
+        expect(simpleCalculator({a: 5, b: 2, action: Action.Exponentiate})).toBe(25);
+    });
 
-  test('should return null for invalid action', () => {
-    // Write your test here
-  });
+    test('should return null for invalid action', () => {
+        expect(simpleCalculator({a: 5, b: 2, action: ':'})).toBe(null);
+    });
 
-  test('should return null for invalid arguments', () => {
-    // Write your test here
-  });
+    test('should return null for invalid arguments', () => {
+        expect(simpleCalculator({a: '5', b: 2, action: Action.Subtract})).toBe(null);
+    });
 });
+
+
