e35d78147240b58b07000aad27a56079b974c826
George Erokhin <Georgii070684@gmail.com>
2024-03-15 11:35:48 +0300

    export class BankAccount test

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,44 +1,54 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import {
+    // getBankAccount,
+    BankAccount, InsufficientFundsError
+} from '.';
 
 describe('BankAccount', () => {
-  test('should create account with initial balance', () => {
-    // Write your test here
-  });
+    test('should create account with initial balance', () => {
+        const balance = 100500
+        const acc = new BankAccount(balance)
+        expect(acc.getBalance()).toBe(balance)
+    });
 
-  test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
-  });
+    test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
+        const balance = 100500
+        const acc = new BankAccount(balance)
+        expect(() => acc.withdraw(100505)).toThrow(InsufficientFundsError)
+    });
 
-  test('should throw error when transferring more than balance', () => {
-    // Write your test here
-  });
+    test('should throw error when transferring more than balance', () => {
+        const balance = 100500
+        const acc = new BankAccount(balance)
+        const toAcc = new BankAccount(balance)
+        expect(() => acc.transfer(100505, toAcc)).toThrow(InsufficientFundsError)
+    });
 
-  test('should throw error when transferring to the same account', () => {
-    // Write your test here
-  });
+    test('should throw error when transferring to the same account', () => {
+        // Write your test here
+    });
 
-  test('should deposit money', () => {
-    // Write your test here
-  });
+    test('should deposit money', () => {
+        // Write your test here
+    });
 
-  test('should withdraw money', () => {
-    // Write your test here
-  });
+    test('should withdraw money', () => {
+        // Write your test here
+    });
 
-  test('should transfer money', () => {
-    // Write your test here
-  });
+    test('should transfer money', () => {
+        // Write your test here
+    });
 
-  test('fetchBalance should return number in case if request did not failed', async () => {
-    // Write your tests here
-  });
+    test('fetchBalance should return number in case if request did not failed', async () => {
+        // Write your tests here
+    });
 
-  test('should set new balance if fetchBalance returned number', async () => {
-    // Write your tests here
-  });
+    test('should set new balance if fetchBalance returned number', async () => {
+        // Write your tests here
+    });
 
-  test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
-    // Write your tests here
-  });
+    test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
+        // Write your tests here
+    });
 });
